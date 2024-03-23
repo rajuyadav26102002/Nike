51d0e96327d6f4b84ac193b8e905ead7073e17b0
Raju_Yadav <128674414+rajuyadav26102002@users.noreply.github.com>
2024-03-23 16:57:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+gsap.from("#nav-part1 ",{
+    delay : 0.5,
+    opacity : 0,
+    duration : 1,
+    y:-60,
+
+
+})
+gsap.from("#nav-part2",{
+    delay : 0.5,
+    opacity : 0,
+    duration : 1,
+    x:60
+})
+
+gsap.from(".card1",{
+    delay : 0.5,
+    opacity : 0,
+    duration : 1,
+    y:-60
+})
+gsap.from(".card2",{
+    delay : 0.6,
+    opacity : 0,
+    duration : 1,
+    y:60
+})
+gsap.from(".card3",{
+    delay : 0.7,
+    opacity : 0,
+    duration : 1,
+    y:-60
+})
+gsap.from(".card4",{
+    delay : 0.8,
+    opacity : 0,
+    duration : 1,
+    y:60
+})
+
+gsap.from("#left",{
+    delay : 0.5,
+    opacity : 0,
+    duration : 1,
+    x:-60
+})
+
+gsap.from("button",{
+    delay : 0.7,
+    opacity : 0,
+    duration : 1,
+    rotate : 360
+})
